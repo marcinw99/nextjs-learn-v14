7e5b7c8c6d5439dbbe23468cb4a650905e49400d
Marcin Wąsik <marcinmw99@gmail.com>
2024-05-01 14:11:50 +0200

    feat: form validation and accessiblity

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,20 +8,43 @@ import {redirect} from "next/navigation";
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: "Please select a customer"
+    }),
+    amount: z.coerce.number().gt(0, {message: "Please enter a n amount greater than 0."}),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: "Please select an invoice status."
+    }),
     date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({id: true, date: true});
 
-export const createInvoice = async (formData: FormData) => {
-    const {customerId, amount, status} = CreateInvoice.parse({
+// This is temporary until @types/react-dom is updated
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+
+export const createInvoice = async (prevState: State, formData: FormData) => {
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     })
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+
+    const {customerId, amount, status} = validatedFields.data
 
     const amountInCents = Math.floor(amount * 100);
     const date = new Date().toISOString().split('T')[0];
@@ -75,7 +98,7 @@ export async function deleteInvoice(id: string) {
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
         revalidatePath('/dashboard/invoices');
-        return { message: "Deleted the invoice." }
+        return {message: "Deleted the invoice."}
     } catch (error) {
         return {
             message: "Database Error: Failed to delete invoice",
